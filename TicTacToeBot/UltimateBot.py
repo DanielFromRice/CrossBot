45c532aed7825feea090cec78e9eb94093f8ef62
Daniel Rothfusz <dpr7@rice.edu>
2020-12-29 11:30:27 -0600

    Formatting/help text

--- TicTacToeBot/UltimateBot.py
+++ TicTacToeBot/UltimateBot.py
@@ -55,7 +55,7 @@ async def on_ready():
     print(f'{ticBot.user} has connected to Discord on the following guilds:')
     for guild in ticBot.guilds:
         print(f'{guild.name}  (id: {guild.id})')
-    # await ticBot.change_presence(activity=discord.Game(name="a game"))
+    await ticBot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="\"xo [command]\""))
 
 
 @ticBot.command(name="new", help="Start a new Tic Tac Toe Game")
@@ -75,14 +75,18 @@ async def new_game(ctx, member_in: discord.Member):
         move_list = []
         sector_statuses = {key: "N" for key in range(9)}
         if game_data.keys().__contains__(str(player1.id)):
-            game_data[str(player1.id)][str(player2.id)] = {"moves": move_list, "turn": True, "sym": "X", "secs": sector_statuses}
+            game_data[str(player1.id)][str(player2.id)] = \
+                {"moves": move_list, "turn": True, "sym": "X", "secs": sector_statuses}
         else:
-            game_data[str(player1.id)] = {str(player2.id): {"moves": move_list, "turn": True, "sym": "X", "secs": sector_statuses}}
+            game_data[str(player1.id)] = \
+                {str(player2.id): {"moves": move_list, "turn": True, "sym": "X", "secs": sector_statuses}}
         if game_data.keys().__contains__(str(player2.id)):
-            game_data[str(player2.id)][str(player1.id)] = {"moves": move_list, "turn": False, "sym": "O", "secs": sector_statuses}
+            game_data[str(player2.id)][str(player1.id)] = \
+                {"moves": move_list, "turn": False, "sym": "O", "secs": sector_statuses}
         else:
-            game_data[str(player2.id)] = {str(player1.id): {"moves": move_list, "turn": False, "sym": "O", "secs": sector_statuses}}
-        print(game_data)
+            game_data[str(player2.id)] = \
+                {str(player1.id): {"moves": move_list, "turn": False, "sym": "O", "secs": sector_statuses}}
+        # print(game_data)
         await ctx.message.delete()
         img = UltimateGame.draw_game(move_list, sector_statuses)
         print("I MADE IT")
@@ -103,19 +107,23 @@ async def new_game_error(ctx, err):
             "appropriate arguments in the form: \n> XO.new [@player_name]")
     elif isinstance(err, commands.BadArgument):
         if str(err) == "P1":
-            await ctx.send(f"{ctx.message.author}, you already have a game active with this player! Finish or quit your current game to start a new one")
+            await ctx.send(f"{ctx.message.author}, you already have a game active with this player! Finish or quit your"
+                           f" current game to start a new one")
         elif str(err) == "Same":
             await ctx.send(f"You can't play a game with yourself!")
     else:
         print(err)
 
 
-@ticBot.command(name="play", help="Play your turn!")
+@ticBot.command(name="play", help="Play your turn!\nThe squares are labelled as:\n1 2 3\n4 5 6\n7 8 9\n\nPut the number"
+                                  " for the big square (sector) first, then the little square within it."
+                                  " Then tag your opponent")
 async def play(ctx, sector: int, space: int, opponent: discord.Member):
     player = ctx.message.author
 
     if not game_data.keys().__contains__(str(player.id)) or not game_data.keys().__contains__(str(opponent.id)) or not \
-            game_data[str(player.id)].keys().__contains__(str(opponent.id)) or not game_data[str(opponent.id)].keys().__contains__(str(player.id)):
+            game_data[str(player.id)].keys().__contains__(str(opponent.id)) or not \
+            game_data[str(opponent.id)].keys().__contains__(str(player.id)):
         raise commands.MemberNotFound("No Game Active")
     if not game_data[str(player.id)][str(opponent.id)]["turn"]:
         raise commands.BadBoolArgument("Not your turn")
@@ -126,7 +134,7 @@ async def play(ctx, sector: int, space: int, opponent: discord.Member):
     if len(moves) == 0:
         pass
     else:
-        assigned_sector = moves[len(moves)-1][2]
+        assigned_sector = moves[len(moves) - 1][2]
         print(moves)
         used_spaces = [move[2] for move in moves if move[1] == assigned_sector]
         if len(used_spaces) < 9:
@@ -140,41 +148,51 @@ async def play(ctx, sector: int, space: int, opponent: discord.Member):
     changed_sector = [[move[0], move[2]] for move in moves if move[1] == sector - 1]
     sector_win = UltimateGame.check_win(changed_sector)
     winner = "N"
-    print("made it a")
     if sector_win != "N":
         game_data[str(player.id)][str(opponent.id)]["secs"][sector - 1] = sector_win
         game_data[str(opponent.id)][str(player.id)]["secs"][sector - 1] = sector_win
-        print("made it b")
-        sector_list = [[sym, sec] for sec, sym in game_data[str(player.id)][str(opponent.id)]["secs"].items() if sym != "N"]
-        # print("SECTORS")
+        sector_list = [[sym, sec] for sec, sym in game_data[str(player.id)][str(opponent.id)]["secs"].items() if
+                       sym != "N"]
         winner = UltimateGame.check_win(sector_list)
-        # print(winner)
-    print("made it c")
-    if winner == "N":
-        print("made it 1")
+    if winner != "N":
+        # Someone won
+        img = UltimateGame.draw_game(moves, game_data[str(player.id)][str(opponent.id)]["secs"])
+        with io.BytesIO() as image_binary:
+            img.save(image_binary, 'PNG')
+            image_binary.seek(0)
+            await ctx.send(
+                f"{winner}'s won!!! Congratulations {player}!!! Better luck next time, {opponent}. To play again, use "
+                f"the command \"xo new @[opponent]\"",
+                file=discord.File(fp=image_binary, filename='latest_turn.png'))
+        await ctx.message.delete()
+        game_data[str(player.id)].pop(str(opponent.id))
+        game_data[str(opponent.id)].pop(str(player.id))
+    elif len(moves) == 81:
+        # Game is a tie
         img = UltimateGame.draw_game(moves, game_data[str(player.id)][str(opponent.id)]["secs"])
         with io.BytesIO() as image_binary:
             img.save(image_binary, 'PNG')
             image_binary.seek(0)
             await ctx.send(f"----------------------------------------------")
-            await ctx.send(f"{ctx.message.author} just played an {game_data[str(player.id)][str(opponent.id)]['sym']}!", file=discord.File(fp=image_binary, filename='latest_turn.png'))
+            await ctx.send(f"Good game {ctx.message.author} and {opponent}! Your game ended with a tie!")
+            await ctx.message.delete()
+            game_data[str(player.id)].pop(str(opponent.id))
+            game_data[str(opponent.id)].pop(str(player.id))
+    else:
+        # Game continues
+        img = UltimateGame.draw_game(moves, game_data[str(player.id)][str(opponent.id)]["secs"])
+        with io.BytesIO() as image_binary:
+            img.save(image_binary, 'PNG')
+            image_binary.seek(0)
+            await ctx.send(f"----------------------------------------------")
+            await ctx.send(f"{ctx.message.author} just played an {game_data[str(player.id)][str(opponent.id)]['sym']}!",
+                           file=discord.File(fp=image_binary, filename='latest_turn.png'))
             await ctx.send(f"{opponent}, it is your turn!")
-        print("made it 2")
         await ctx.message.delete()
         game_data[str(player.id)][str(opponent.id)]["turn"] = False
         game_data[str(opponent.id)][str(player.id)]["turn"] = True
         game_data[str(player.id)][str(opponent.id)]["moves"] = moves
         game_data[str(opponent.id)][str(player.id)]["moves"] = moves
-    else:
-        img = UltimateGame.draw_game(moves, game_data[str(player.id)][str(opponent.id)]["secs"])
-        with io.BytesIO() as image_binary:
-            img.save(image_binary, 'PNG')
-            image_binary.seek(0)
-            await ctx.send(f"{winner}'s won! Congratulations {player}!! Better luck next time, {opponent}",
-                           file=discord.File(fp=image_binary, filename='latest_turn.png'))
-        await ctx.message.delete()
-        game_data[str(player.id)].pop(str(opponent.id))
-        game_data[str(opponent.id)].pop(str(player.id))
     await write_data()
 
 
@@ -186,7 +204,9 @@ async def play_error(ctx, err):
         await ctx.send(f"{ctx.message.author}, it's not your turn!")
     elif isinstance(err, commands.ArgumentParsingError):
         if str(err) == "Wrong Sector":
-            await ctx.send(f"Might want to rethink that one! You are trying to play in the wrong sector! \nRemember to write your move as XO.play <big_grid> <small_grid> @<opponent>")
+            await ctx.send(
+                f"Might want to rethink that one! You are trying to play in the wrong sector! \nRemember to write your "
+                f"move as XO.play <big_grid> <small_grid> @<opponent>")
         else:
             await ctx.send(f"You can't play on that space!")
     elif isinstance(err, commands.MissingRequiredArgument):
@@ -197,23 +217,26 @@ async def play_error(ctx, err):
         print(err)
 
 
-@ticBot.command(name="game_info", help="Current information about your active game")
+@ticBot.command(name="info", help="Displays all of your current games")
 async def game_info(ctx):
     global game_data
     player = ctx.message.author
     if str(player.id) in game_data.keys():
         if len(game_data[str(player.id)]) > 0:
-            await ctx.send(f"Hi {player}! You have the following active games:")
+            await ctx.send(f"Hi {player.display_name}! You have the following active games:")
             for opp_id in game_data[str(player.id)]:
                 if game_data[str(player.id)][str(opp_id)]["turn"]:
                     whose_turn = "your"
                 else:
                     whose_turn = "their"
-                img = UltimateGame.draw_game(game_data[str(player.id)][str(opp_id)]["moves"], game_data[str(player.id)][str(opp_id)]["secs"])
+                img = UltimateGame.draw_game(game_data[str(player.id)][str(opp_id)]["moves"],
+                                             game_data[str(player.id)][str(opp_id)]["secs"])
                 with io.BytesIO() as image_binary:
                     img.save(image_binary, 'PNG')
                     image_binary.seek(0)
-                    await ctx.send(f"You are currently playing {ticBot.get_user(int(opp_id))} and it is {whose_turn} turn. You are playing as {game_data[str(player.id)][opp_id]['sym']}.")
+                    await ctx.send(
+                        f"You are currently playing {ticBot.get_user(int(opp_id))} and it is {whose_turn} turn. "
+                        f"You are playing as {game_data[str(player.id)][opp_id]['sym']}.")
                     await ctx.send(file=discord.File(fp=image_binary, filename="latest_turn.png"))
         else:
             await ctx.send(f"{player}, you have no active games!")
@@ -221,17 +244,16 @@ async def game_info(ctx):
         await ctx.send(f"{player}, you have no active games!")
 
 
-@ticBot.command(name="quit", help="Quits a current game")
-async def quit(ctx, other_player: discord.Member):
+@ticBot.command(name="resign", help="Resigns your current game")
+async def resign(ctx, other_player: discord.Member):
     player = ctx.message.author
     if game_data[str(player.id)].__contains__(str(other_player.id)):
         game_data[str(other_player.id)].pop(str(player.id))
         game_data[str(player.id)].pop(str(other_player.id))
-        await ctx.send(f"{player}, you have quit your game against {other_player}")
+        await ctx.send(f"Congrats {other_player}, {player} has resigned from your game!!")
         await write_data()
     else:
         await ctx.send(f"{player}, you aren't currently playing against {other_player}!")
 
 
 ticBot.run(TOKEN)
-
